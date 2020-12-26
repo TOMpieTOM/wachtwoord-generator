687e36b3efaf5a0f149e0e3a09fb3941589fd1d8
TOMpieTOM <72800374+TOMpieTOM@users.noreply.github.com>
2020-12-26 13:31:20 +0100

    yes

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import random
 
+uppercase_letters = "ABCDEFGIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "1234567890"
+symbols = "!@#$^&*<>.,?"
+
+upper, lower, nums, syms = True, True, True, True
+
+all = ""
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
+
+lenght = 50
+amount = 100
+
+for x in range(amount):
+    password = "".join(random.sample(all, lenght))
+    print(password)
